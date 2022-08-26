2a323ca3af63b5ceaa8b0d7f46117ab5dfdbc88b
shubhamyadav2000 <shubham.yadav@kuchanaya.com>
2022-08-26 19:20:32 +0530

    first commit

--- src/services/AppCommonStore.tsx
+++ src/services/AppCommonStore.tsx
@@ -1,25 +1,26 @@
 import { createSignal } from "solid-js";
 
 const [repoCollection, setRepoCollection] = createSignal([]);
-const [userInfo, setUserInfo] = createSignal({}||
-  {avatar_url: "",
-  bio: "",
-  blog: "",
-  company: "",
-  email: "",
-  location: "",
-  login: "",
-  name: "",
-  public_gists: 0,
-  public_repos: 0,
-  updated_at: "",
-  url: "",
-  following: 0,
-  followers: 0,
-  hireable: "",
-  html_url: "", 
-}
-  );
+const [userInfo, setUserInfo] = createSignal(
+  {} || {
+    avatar_url: "",
+    bio: "",
+    blog: "",
+    company: "",
+    email: "",
+    location: "",
+    login: "",
+    name: "",
+    public_gists: 0,
+    public_repos: 0,
+    updated_at: "",
+    url: "",
+    following: 0,
+    followers: 0,
+    hireable: "",
+    html_url: "",
+  }
+);
 
 const getRepository = (userName: string) => {
   getUserInfo(userName);
